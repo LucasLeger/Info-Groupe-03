f6514e7b42781cf813ad9a655f9605fc7001add2
Squirrhell <32261427+Squirrhell@users.noreply.github.com>
2021-02-02 16:24:36 +0100

    Update main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -6,7 +6,7 @@ function addInput() {
     var original = document.getElementById('div');
     
     var newInput = document.createElement('input');
-    newInput.class = "boite";
+    newInput.className = "boite";
     newInput.type = "password";
     newInput.name = "code"+document.getElementById('div').children.length;
     newInput.placeholder = "Entrez un code";
