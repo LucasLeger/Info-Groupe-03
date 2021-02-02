9fec4fe2ff832f048695edf93f74fd3f0ad69429
Squirrhell <32261427+Squirrhell@users.noreply.github.com>
2021-02-02 13:47:14 +0100

    Update connectBdd.php

--- docs/php/connect/connectBdd.php
+++ docs/php/connect/connectBdd.php
@@ -5,7 +5,7 @@ function connectBDD() {
     $port = '3306';
     $db = 'sqirl_g348h';
     $login = 'sqirl';
-    $password = '';
+    $password = 'spaqssiworrdl';
 
     try {
         $pdo = new PDO('mysql:host='.$host.';port='.$port. ';dbname='.$db, $login, $password);
