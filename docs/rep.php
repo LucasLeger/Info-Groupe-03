b3911e6b83fa541d85f3e689c88d32ac420065e5
Squirrhell <32261427+Squirrhell@users.noreply.github.com>
2021-02-02 17:05:32 +0100

    Update rep.php

--- docs/rep.php
+++ docs/rep.php
@@ -36,7 +36,7 @@
    <script src="js/main.js"></script>
    </head>
    <body>
-            <form action="php/connect/save.php" method="post">
+            <form action="php/save.php" method="post">
                 <div class="code" >
                     <div id="div">
                         <input class="boite" type="password" name="code0" placeholder="Entrez un code" required>
